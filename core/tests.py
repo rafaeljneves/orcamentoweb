926ec091fc5ca5945dc5796e07162e19b4d886ed
rafaeljneves <rafaeljneves@gmail.com>
2021-08-10 03:25:26 -0300

    Ajustando testes

--- core/tests.py
+++ core/tests.py
@@ -3,12 +3,13 @@ from .models import ItemOrcamento
 
 class TestItemOrcamento(TestCase):
     def setUp(self):
-        self.itemOrcamento = ItemOrcamento.objects.create(
-            descricao = 'Caneta esferográfica',
-            valor = 2.50
-        )
+        ItemOrcamento.objects.create(descricao = 'Caneta esferográfica', valor = 2.50)
 
     def test_item_is_created(self):
-        item = ItemOrcamento.objects.create(itemOrcamento=self.itemOrcamento)
+        item = ItemOrcamento.objects.get(id=1)
+        self.assertEqual(item.descricao,"Caneta esferográfica")
 
-        self.assertTrue(ItemOrcamento.objects.exists())
+    def test_nome_campo_descricao(self):
+        item = ItemOrcamento.objects.get(id=1)
+        campo_descricao = item._meta.get_field('descricao').verbose_name
+        self.assertEquals(campo_descricao, 'descricao')
